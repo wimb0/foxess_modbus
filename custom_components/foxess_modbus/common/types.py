0ffe0ec10a6fcee0bf14952e05e3c5ea813e74ff
wimb0 <11030068+wimb0@users.noreply.github.com>
2025-10-10 08:13:51 +0200

    Add MQ2200-M-A to supported types

--- custom_components/foxess_modbus/common/types.py
+++ custom_components/foxess_modbus/common/types.py
@@ -59,6 +59,8 @@ class InverterModel(StrEnum):
     H3_PRO = "H3_PRO"
     H3_SMART = "H3_SMART"
 
+    MQ2200-M-A = "MQ2200-M-A"
+
 
 class Inv(Flag):
     """
@@ -90,6 +92,8 @@ class Inv(Flag):
     H3_PRO_SET = H3_PRO_PRE122 | H3_PRO_122
 
     H3_SMART = auto()
+
+    MQ2200-M-A = auto()
 
     ALL = H1_LAN | H1_G1 | H1_G2_SET | KH_SET | H3_SET | H3_PRO_SET | H3_SMART
 
